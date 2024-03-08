bcf9c970a5beabdb86367795ab0499a5097ba333
vankien268 <vovinamqnqy@gmail.com>
2024-03-08 15:16:30 +0700

    update

---
+++ routes/channels.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Here you may register all of the event broadcasting channels that your
+| application supports. The given channel authorization callbacks are
+| used to check if an authenticated user can listen to the channel.
+|
+*/
+
+use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('App.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('room.{id}', function ($user, $id) {
+    $user->new_messages = 0;
+    return $user;
+});
